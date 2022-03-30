5f7e1c2dcc0b3cb0f66d69d36263491869716032
= <maria.louisa.castro@gmail.com>
2022-03-30 17:51:08 +0100

    Add function to display video duration
    Relates #6

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const video = document.querySelector("#video");
+const video = document.querySelector("#video1");
 let playing = false;
+let length;
 
+// Play & Pause functionality
 document.querySelector("#play").addEventListener("click", () => {
   playing ? video.pause() : video.play();
+  //change playing varible for ability to toggle
   playing ? (playing = false) : (playing = true);
+  if (!length) showDuration();
 });
+
+const showDuration = () => {
+  length = video.duration;
+  //turn seconds into mins
+  let mins = Math.floor(length / 60);
+  let secs = Math.round(length - mins * 60);
+  if (mins < 10) mins = "0" + mins;
+  if (secs < 10) mins = "0" + secs;
+  document.querySelector(".duration").innerHTML = `${mins}:${secs}`;
+};
